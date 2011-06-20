644252afcafb7fc06cca23e0ab5faea180895583
guenter.platzerworld@gmail.com <guenter.platzerworld@gmail.com@25a68971-6cc2-a3b7-306a-3c0fbd43d85c>
2011-06-20 10:02:38 +0000

    one to many

--- trunk/platzerworld-kegelverwaltung/src/com/platzerworld/kegelverwaltung/model/Mannschaft.java
+++ trunk/platzerworld-kegelverwaltung/src/com/platzerworld/kegelverwaltung/model/Mannschaft.java
@@ -1,11 +1,15 @@
 package com.platzerworld.kegelverwaltung.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Mannschaft {
@@ -15,7 +19,9 @@ public class Mannschaft {
 	private Long id;
 	private String userId;
 	private String name;
-	private Date lastChangedDate;
+	private Date lastChangedDate;	
+	private Klasse klasse;
+	private List<Spieler> spielerList = new ArrayList<Spieler>();
 	
 	public Mannschaft(String userId, String name, Date lastChangedDate){
 		this.userId = userId;
@@ -42,9 +48,31 @@ public class Mannschaft {
 	public Long getId() {
 		return id;
 	}
-	
+		
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
 	public String getUserId() {
 		return userId;
 	}
 
+	@ManyToOne
+	public Klasse getKlasse() {
+		return klasse;
+	}
+
+	public void setKlasse(Klasse klasse) {
+		this.klasse = klasse;
+	}
+	
+	@OneToMany
+	public List<Spieler> getJobList() {
+		return this.spielerList;
+	}
+
+	public void setJobList(List<Spieler> nickName) {
+		this.spielerList = nickName;
+	}
+	
 }
